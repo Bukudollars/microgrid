19bee2f9e67d2b1106e8723ecd1305c32644f767
James <james.wang12345@gmail.com>
2024-09-20 15:49:23 -0500

    Add PV Simulation Component

--- src/dashboard/simulation/PV.jsx
+++ src/dashboard/simulation/PV.jsx
@@ -1,13 +1,24 @@
 import * as React from 'react';
-import { Box } from '@mui/material';
+import { Box, Paper, Tooltip } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
 function PV() {
     return (
-        <Box>
-            <Typography variant="h5">PV</Typography>
-            <img src="./pv.png" alt="PV placeholder" />
-        </Box>
+        <Paper elevation={4}>
+            <Box sx={{margin: 2, padding: 2, textAlign: 'left'}}>
+                <Typography variant="h5">PV</Typography>
+                <Tooltip title="Real Load" arrow>
+                    <Typography variant="body1">(P) 386 kW</Typography>
+                </Tooltip>
+                <Tooltip title="Reactive Load" arrow>
+                    <Typography variant="body1">(Q) 0.00 kVAr 16/16</Typography>
+                </Tooltip>
+                <Tooltip title="Power Factor" arrow>
+                    <Typography variant="body1">(PF) 1</Typography>
+                </Tooltip>
+                {/* <img src="./pv.png" alt="PV placeholder" /> */}
+            </Box>
+        </Paper>
     );
 }
 
