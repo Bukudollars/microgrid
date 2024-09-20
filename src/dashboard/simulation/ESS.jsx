1d51e9498e0c5bf1e9e6fa522ca1c2d12d11c7d2
James <james.wang12345@gmail.com>
2024-09-20 16:06:34 -0500

    Add ESS Simulation Component

--- src/dashboard/simulation/ESS.jsx
+++ src/dashboard/simulation/ESS.jsx
@@ -1,13 +1,24 @@
 import * as React from 'react';
-import { Box } from '@mui/material';
+import { Box, Tooltip, Paper } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
 function ESS() {
     return (
-        <Box>
-            <Typography variant="h5">ESS</Typography>
-            <img src="./ess.png" alt="ESS placeholder" />
-        </Box>
+        <Paper elevation={4}>
+            <Box sx={{textAlign: 'left', padding: 2, margin: 2}}>
+                <Typography variant="h5">ESS</Typography>
+                <Tooltip title="Real Load" arrow>
+                        <Typography variant="body1">(P) 446 kW</Typography>
+                    </Tooltip>
+                    <Tooltip title="Reactive Load" arrow>
+                        <Typography variant="body1">(Q) 0.00 kVAr</Typography>
+                    </Tooltip>
+                    <Tooltip title="Power Factor" arrow>
+                        <Typography variant="body1">(PF) 1 56.96%</Typography>
+                    </Tooltip>
+                {/* <img src="./ess.png" alt="ESS placeholder" /> */}
+            </Box>
+        </Paper>
     );
 }
 export default ESS;
