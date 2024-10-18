8622b63ad760081ff2f12cf1c9d394770d753a22
Adi-K527 <adikandakurtikar2@gmail.com>
2024-10-18 10:55:04 -0500

    simulation components

--- src/dashboard/simulation/ESS.jsx
+++ src/dashboard/simulation/ESS.jsx
@@ -1,20 +1,27 @@
 import * as React from 'react';
 import { Box, Tooltip, Paper } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import { useSimulationState } from '../../contexts/SimulationContext';
 
 function ESS() {
+    const { simulationData, loading, currentIndex } = useSimulationState();
+    const validData = simulationData.length > 0 && currentIndex < simulationData.length && !loading;
+    const realLoad =  validData ? simulationData[currentIndex].essRealPowerContribution : 0;
+    const reactiveLoad = validData ? simulationData[currentIndex].essReactivePowerContribution : 0;
+    const powerFactor = validData ? simulationData[currentIndex].essPowerFactor : 0;
+
     return (
         <Paper elevation={4}>
             <Box sx={{textAlign: 'left', padding: 2, margin: 2}}>
                 <Typography variant="h5">ESS</Typography>
                 <Tooltip title="Real Load" arrow>
-                        <Typography variant="body1">(P) 446 kW</Typography>
+                        <Typography variant="body1">(P) {realLoad.toFixed(2)} kW</Typography>
                     </Tooltip>
                     <Tooltip title="Reactive Load" arrow>
-                        <Typography variant="body1">(Q) 0.00 kVAr</Typography>
+                        <Typography variant="body1">(Q) {reactiveLoad.toFixed(2)} kVAr</Typography>
                     </Tooltip>
                     <Tooltip title="Power Factor" arrow>
-                        <Typography variant="body1">(PF) 1 56.96%</Typography>
+                        <Typography variant="body1">(PF) {powerFactor.toFixed(2)}</Typography>
                     </Tooltip>
                 {/* <img src="./ess.png" alt="ESS placeholder" /> */}
             </Box>
--- src/dashboard/simulation/Gen.jsx
+++ src/dashboard/simulation/Gen.jsx
@@ -1,20 +1,31 @@
 import * as React from 'react';
 import { Box, Paper, Tooltip } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import { useSimulationState } from '../../contexts/SimulationContext';
 
 function Gen() {
+    const { simulationData, loading, currentIndex } = useSimulationState();
+    const validData = simulationData.length > 0 && currentIndex < simulationData.length && !loading;
+    const realLoad =  validData ? simulationData[currentIndex].gensetRealPowerContribution : 0;
+    const reactiveLoad = validData ? simulationData[currentIndex].gensetReactivePowerContribution : 0;
+    const powerFactor = validData ? simulationData[currentIndex].gensetPowerFactor : 0;
+    const gensetsRequired = validData ? simulationData[currentIndex].gensetsRequired : 0;
+
     return (
         <Paper elevation={4}>
             <Box sx={{textAlign: 'left', padding: 2, margin: 2}}>
                 <Typography variant="h5">Gen</Typography>
                 <Tooltip title="Real Load" arrow>
-                    <Typography variant="body1">(P) 135 kW</Typography>
+                    <Typography variant="body1">(P) {realLoad.toFixed(2)} kW</Typography>
                 </Tooltip>
                 <Tooltip title="Reactive Load" arrow>
-                    <Typography variant="body1">(Q) 0.00 kVAr 1/5</Typography>
+                    <Typography variant="body1">(Q) {reactiveLoad.toFixed(2)} kVAr 1/5</Typography>
                 </Tooltip>
                 <Tooltip title="Power Factor" arrow>
-                    <Typography variant="body1">(PF) 0.00</Typography>
+                    <Typography variant="body1">(PF) {powerFactor.toFixed(2)}</Typography>
+                </Tooltip>
+                <Tooltip title="Gensets Required" arrow>
+                    <Typography variant="body1">Gensets Required: {gensetsRequired}</Typography>
                 </Tooltip>
                 {/* <img src="./gen.png" alt="Gen placeholder" /> */}
             </Box>
