f4c64fa2b7b438fddd79a26d32444b22faed3ced
James <james.wang12345@gmail.com>
2024-10-26 16:51:20 -0500

    Refactor to Hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import * as React from 'react';
 import { SimulationProvider } from './contexts/SimulationContext';  
 import { SettingsProvider } from './contexts/SettingsContext';
 import Dashboard from './dashboard/Dashboard';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 export default function App() {
   return (
-    <Router basename='/microgrid/'>
+    <HashRouter>
       <SettingsProvider>
         <SimulationProvider>
           <Dashboard />
         </SimulationProvider>
       </SettingsProvider>
-    </Router>
+    </HashRouter>
     
     
   );
